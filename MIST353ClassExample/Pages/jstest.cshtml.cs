fc0b71ef94165b1b1cf849d08c757600878ef641
Joshua Meadows <jtmeadows@mail.wvu.edu>
2024-01-18 09:46:56 -0500

    working api example

---
+++ MIST353ClassExample/Pages/jstest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MIST353ClassExample.Pages
+{
+    public class jstestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
